f9dbb7285b369b25d8ef2697f0c083500e1c25c3
Kim youngmin <dalikim@users.noreply.github.com>
2021-05-15 13:38:09 +0900

    feat: 자동으로 베이스 브랜치를 찾고 해당DB에서 복사합니다 (#3)

--- lib/branch_db/smart_database_environment.rb
+++ lib/branch_db/smart_database_environment.rb
@@ -1,7 +1,12 @@
 module BranchDb
   class SmartDatabaseEnvironment
+    attr_reader :all_base_branches, :merge_bases_of_all_base_branches
+
     def initialize
-      Rails.logger.debug("Use database #{current_database_name}")
+      @all_base_branches = all_base_branch
+      @merge_bases_of_all_base_branches = @all_base_branches.permutation(2).map do |a, b|
+        merge_base(a, b)
+      end.compact.uniq
     end
 
     def user_name
@@ -16,12 +21,30 @@ module BranchDb
       @host ||= config_env.dig('database', 'host').presence || 'localhost'
     end
 
-    def current_database_name(current_branch = nil)
+    def port
+      @port ||= config_env.dig('database', 'port')
+    end
+
+    def database_name
+      return @database_name if @database_name.present?
+
+      if current_database_name == base_database_name || git_changes_count <= 0 || exists_database?(current_database_name)
+        @database_name = current_database_name
+        return @database_name
+      end
+
+      if git_diffs_count(base_branch) <= 0
+        @database_name = base_database_name
+      end
+
+      @database_name
+    end
+
+    def current_database_name
       @current_database_name ||= branch_database_name(current_branch)
     end
-    alias_method :database_name, :current_database_name
 
-    def base_database_name(base_branch)
+    def base_database_name
       @base_database_name ||= branch_database_name(base_branch)
     end
 
@@ -36,6 +59,26 @@ module BranchDb
 
     def current_branch
       @current_branch ||= %x[git rev-parse --abbrev-ref HEAD].strip
+    end
+
+    def base_branch
+      @base_branch if @base_branch.present?
+
+      if all_base_branches.include?(current_branch)
+        @base_branch = current_branch
+        return @base_branch
+      end
+
+      all_base_branches.each do |current_base_branch|
+        current_merge_base = merge_base(current_base_branch, current_branch)
+        unless merge_bases_of_all_base_branches.include?(current_merge_base)
+          @base_branch = current_base_branch
+          return @base_branch
+        end
+      end
+
+      @base_branch = nil
+      @base_branch
     end
 
     #private
@@ -56,8 +99,21 @@ module BranchDb
       %x[git ls-files -mo --exclude-standard -- #{Rails.root}/db | wc -l].strip&.to_i || 0
     end
 
-    def git_diffs_count(base_branch)
-      %x[git diff --cached #{base_branch} --name-only -- #{Rails.root}/db | wc -l].strip&.to_i || 0
+    def git_diffs_count(branch)
+      %x[git diff --cached #{branch} --name-only -- #{Rails.root}/db | wc -l].strip&.to_i || 0
+    end
+
+    def exists_database?(database_name)
+      0 < %x[mysql -uroot -pzmKL4oue/6R4 --skip-column-names --batch -e "SHOW DATABASES LIKE '#{database_name}' | wc -l"].strip&.to_i || 0
+    end
+
+    def all_base_branch
+      %x[git for-each-ref --format='%(refname:short)' refs/heads/ | grep -v '/'].split.compact
+      # 0 < %x[git show-ref refs/heads/#{branch} | wc -l"].strip&.to_i || 0
+    end
+
+    def merge_base(a, b)
+      %x[git merge-base #{a} #{b}]&.strip
     end
 
     def git_all_branches
