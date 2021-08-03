8ad49292a926d2355eb52f4964d01f6957895957
Dali Kim <dalikim@parti.coop>
2021-08-03 13:56:36 +0900

    fix: 브랜치 중에 "\"가 들어가지 않은 것을 base branch로 설정합니다

--- lib/branch_db/smart_database_environment.rb
+++ lib/branch_db/smart_database_environment.rb
@@ -1,11 +1,5 @@
 module BranchDb
   class SmartDatabaseEnvironment
-    attr_reader :all_base_branches
-
-    def initialize
-      @all_base_branches = all_base_branch
-    end
-
     def user_name
       @username ||= config_env.dig('database', 'username')
     end
@@ -28,13 +22,18 @@ module BranchDb
       @database_name = current_database_name
 
       if current_database_name == base_database_name || git_changes_count > 0 || exists_database?(current_database_name)
+        puts "base_database_name : #{near_base_branch.inspect}"
+        puts "git_changes_count : #{git_changes_count}"
+        puts "database_name: #{@database_name}"
         return @database_name
       end
 
       if git_diffs_count(base_branch) <= 0
+        puts "git_diffs_count : #{git_diffs_count(base_branch)}"
         @database_name = base_database_name
       end
 
+      puts "database_name: #{@database_name}"
       @database_name
     end
 
@@ -60,7 +59,7 @@ module BranchDb
     end
 
     def base_branch
-      @base_branch if @base_branch.present?
+      return @base_branch if @base_branch.present?
 
       if all_base_branches.include?(current_branch)
         @base_branch = current_branch
@@ -97,8 +96,10 @@ module BranchDb
       0 < (%x[mysql -u#{user_name} -p#{password} -h #{host} --skip-column-names --batch -e "SHOW DATABASES LIKE '#{database_name}'" | wc -l].strip&.to_i || 0)
     end
 
-    def all_base_branch
-      %x[git for-each-ref --format='%(refname:short)' refs/heads/ | grep -v '/'].split.compact
+    def all_base_branches
+      @all_base_branches ||= %x[git for-each-ref --format='%(refname:short)' refs/heads/ | grep -v '/'].split.compact.select do |branch|
+        exists_database?(branch_database_name(branch)) || %[main master].include?(branch)
+      end.compact
       # 0 < %x[git show-ref refs/heads/#{branch} | wc -l"].strip&.to_i || 0
     end
 
@@ -107,7 +108,7 @@ module BranchDb
     end
 
     def near_base_branch
-      %x[git show-branch -a 2>/dev/null | grep '\*' | grep -v '/' | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//']&.strip
+      %x[git show-branch -a 2>/dev/null | grep '\\*' | grep -E '#{all_base_branches.map{ |branch| "\\[#{branch}" }.join('|')}' | head -n1 | sed 's/.*\\[\\(.*\\)\\].*/\\1/' | sed 's/[\\^~].*//']&.strip
     end
   end
 end
